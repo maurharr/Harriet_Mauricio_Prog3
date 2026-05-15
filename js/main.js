7406ddaa2333ef746973cdd51d09cfd3fb55203b
Mauricio Harriet <138984683+maurharr@users.noreply.github.com>
2026-05-15 20:09:25 -0300

    Agregado de la logica de los botones y el muestreo de las proximas y anteriores 6 cartas

--- js/main.js
+++ js/main.js
@@ -1,21 +1,61 @@
-const arrayCartas = []
+let paginas = []
+let paginaActual = 0
 
 window.addEventListener("load", async() =>{
+    const siguiente = document.getElementById("siguiente")
+    const anterior = document.getElementById("anterior")
+    
+    cargarCartas()
+
+    siguiente.addEventListener("click", async ()=>{
+        paginaActual++
+        
+        if(paginas[paginaActual]){
+            mostrarCartas(paginas[paginaActual])
+        }else{
+            await cargarCartas()
+        }
+    })
+
+    anterior.addEventListener("click", async ()=>{
+        paginaActual--
+
+        if(paginas[paginaActual]){
+            mostrarCartas(paginas[paginaActual])
+        }else{
+            paginaActual = 0
+        }
+    })    
+
+    
+})
+
+const cargarCartas = async () => {
     const respon = await fetch("https://deckofcardsapi.com/api/deck/new/draw/?count=6")
     const data = await respon.json()
-    console.log(data)
 
-    data.cards.forEach(carta => {
-        arrayCartas.push(new Carta(
+    const nuevasCartas = data.cards.map(carta =>
+        new Carta(
             carta.code,
             carta.value,
             carta.suit,
             carta.image
-        ))
-    });
-    arrayCartas.forEach((carta)=>{
-        const cartas = document.getElementById("cartas")
+        )
+    )
+
+    paginas.push(nuevasCartas)
+    mostrarCartas(nuevasCartas)
+
+    console.log(paginas)
+}
+        
+
+const mostrarCartas = (cartasArray) => {
+    const cartas = document.getElementById("cartas")
+
+    cartas.innerHTML=""
+
+    cartasArray.forEach((carta)=>{
         cartas.appendChild(carta.createHtmlElement())
     })
-    console.log(arrayCartas)
-})
+}
