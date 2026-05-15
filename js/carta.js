2c9d5098cfaf476a103319b53c481478b53a0a0e
Mauricio Harriet <138984683+maurharr@users.noreply.github.com>
2026-05-15 19:38:30 -0300

    Agregado de sus metodos

--- js/carta.js
+++ js/carta.js
@@ -13,4 +13,29 @@ class Carta{
         this.suit = suit
         this.imagen = imagen
     }
+    
+    toJsonString(){
+        return JSON.stringify(this)
+    }
+
+    static createFromJsonString(json){
+        let carta = JSON.parse(json)
+        return new Carta(
+            carta.code,
+            carta.value,
+            carta.suit,
+            carta.imagen
+        )
+    }
+
+    createHtmlElement(){
+        const div = document.createElement("div")
+
+        div.innerHTML=`
+        <h5>${this.code} ${this.suit}</h5>
+        <img src="${this.imagen}">
+        <p>Valor: $${this.value}</p>
+        `
+        return div;
+    }
 }
